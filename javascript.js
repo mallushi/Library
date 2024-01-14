53ea4097ee6080d8643ca6c96c8f2624b2f3d7f7
migena <migena.allushi1@gmail.com>
2024-01-14 19:51:12 +0100

    display book instances after adding to array

--- javascript.js
+++ javascript.js
@@ -9,6 +9,16 @@ function Book(author, title, pgnumb, read){
     this.read = read;
 }
 
+function addBookToLibrary(myLibrary){
+    document.getElementById('libraryContainer').innerHTML = '';
+
+    for(i = 0; i < myLibrary.length; i++){
+        let card = document.createElement('div');
+        card.textContent += JSON.stringify(myLibrary[i]);
+        document.getElementById('libraryContainer').appendChild(card);
+    }
+}
+
 //get user input 
 let dialog = document.getElementById('newBook');
 
@@ -27,12 +37,23 @@ document.getElementById('bookForm').addEventListener('submit', function(event){
     let author = document.getElementById('author').value;
     let title = document.getElementById('title').value;
     let pgnumb = document.getElementById('pgnumb').value;
-    let read = document.getElementById('read').value;
+    let read = document.getElementById('read').checked;
+
     //create a new book instance
     let book1 = new Book(author, title, pgnumb, read);
-    //add the book instance to myLibrary
+
+    //add the book instance to myLibrary array
     myLibrary.push(book1);
-    console.log(book1);
+
+    //add book to the library display
+    addBookToLibrary(myLibrary);
+
+    //clear content after submission
+    document.getElementById('author').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('pgnumb').value = '';
+    document.getElementById('read').checked = '';
+
+    //close the dialog afer submission
     dialog.close();
 });
-
