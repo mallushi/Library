3de125f79056708ae74252b6ec9e5289a1f21148
migena <migena.allushi1@gmail.com>
2024-01-14 17:02:04 +0100

    handle form submission

--- javascript.js
+++ javascript.js
@@ -1,20 +1,38 @@
 //initialize an empty array to store user input
 const myLibrary = [];
 
-function Book(author, title, number, read){
+//define book constructor
+function Book(author, title, pgnumb, read){
     this.author = author;
     this.title = title;
-    this.number = number;
+    this.pgnumb = pgnumb;
     this.read = read;
 }
 
-//function to get user input and store it in the array;
-//function addBookToLibrary(){
-    let dialog = document.getElementById('newBook');
-    document.getElementById('show').onclick = function(){
-        dialog.showModal();
-    };
-    document.getElementById('hide').onclick = function(){
-        dialog.close();
-    };
-//};
+//get user input 
+let dialog = document.getElementById('newBook');
+
+document.getElementById('show').onclick = function(){
+    dialog.showModal();
+};
+document.getElementById('hide').onclick = function(){
+    dialog.close();
+};
+
+//handle form submission
+document.getElementById('bookForm').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    //get form values
+    let author = document.getElementById('author').value;
+    let title = document.getElementById('title').value;
+    let pgnumb = document.getElementById('pgnumb').value;
+    let read = document.getElementById('read').value;
+    //create a new book instance
+    let book1 = new Book(author, title, pgnumb, read);
+    //add the book instance to myLibrary
+    myLibrary.push(book1);
+    console.log(book1);
+    dialog.close();
+});
+
